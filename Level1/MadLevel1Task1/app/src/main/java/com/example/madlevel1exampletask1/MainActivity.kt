1ca7726e58cf524a98ec1ffec8f008d52035e0b3
Lawrance <Lawrance.bahem@hva.nl>
2021-02-10 20:14:01 +0100

    code changes

--- Level1/MadLevel1Task1/app/src/main/java/com/example/madlevel1exampletask1/MainActivity.kt
+++ Level1/MadLevel1Task1/app/src/main/java/com/example/madlevel1exampletask1/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.madlevel1exampletask1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.madlevel1exampletask1.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
     private var currentThrow: Int = 1
@@ -21,16 +22,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        binding.lowerBtn.setOnClickListener {
-            onLowClick()
-        }
-        binding.higherBtn.setOnClickListener {
-            onHighClick()
-        }
+        binding.lowerBtn.setOnClickListener(this)
+        binding.higherBtn.setOnClickListener(this)
+        binding.equalBtn.setOnClickListener(this)
 
-        binding.equalBtn.setOnClickListener {
-            onEqual()
-        }
         //update the user interface
         updateUI()
     }
@@ -44,39 +39,10 @@ class MainActivity : AppCompatActivity() {
         binding.imageView.setImageResource(resources.getIdentifier("dice${currentThrow}", "drawable", packageName))
     }
 
-    private fun onHighClick() {
-        rollDice()
-        if (currentThrow > lastThrow){
-            showMessage(getString(R.string.correct))
-        }else{
-            showMessage(getString(R.string.incorrect))
-        }
-    }
-    private fun onLowClick() {
-        rollDice()
-        if (currentThrow < lastThrow){
-            showMessage(getString(R.string.correct))
-        }else{
-            showMessage(getString(R.string.incorrect))
-        }
-    }
-
-    /**
-     * Once the equal button clicked
-     */
-    private fun onEqual() {
-        rollDice()
-        if (currentThrow == lastThrow){
-            showMessage(getString(R.string.correct))
-        }else{
-            showMessage(getString(R.string.incorrect))
-        }
-    }
-
     /**
      * To get a random score
      */
-    private fun rollDice(){
+    private fun rollDice() {
         lastThrow = currentThrow;
         currentThrow = (1..6).random()
 
@@ -86,8 +52,23 @@ class MainActivity : AppCompatActivity() {
     /**
      * To show a toast correct message
      */
-    private fun showMessage(message:String){
-        Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
+    /**
+     * To handle all click buttons
+     */
+    override fun onClick(v: View?) {
+        rollDice()
+        val correct = when (v!!.id) {
+            binding.equalBtn.id -> currentThrow == lastThrow
+            binding.higherBtn.id -> currentThrow > lastThrow
+            else -> currentThrow < lastThrow
+        }
+        when (correct) {
+            true -> showMessage(getString(R.string.correct))
+            else -> showMessage(getString(R.string.incorrect))
+        }
+    }
 }
